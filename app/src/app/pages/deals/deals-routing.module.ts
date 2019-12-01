61b539584501ac5ea0f67f2596c58464bc6c7c2d
Caio <caiooliveirabraz@hotmail.com>
2019-12-01 12:49:54 -0400

    cadastro de ofertas
    - interface de cadastro

--- app/src/app/pages/deals/deals-routing.module.ts
+++ app/src/app/pages/deals/deals-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DealListComponent} from './deal-list/deal-list.component';
 import {DealDetailComponent} from "./deal-detail/deal-detail.component";
+import {DealFormComponent} from "./deal-form/deal-form.component";
 
 const routes: Routes = [
   {path: '', component: DealListComponent},
-  {path: ':id', component: DealDetailComponent}
+  {path: 'view/:id', component: DealDetailComponent},
+  {path: 'new', component: DealFormComponent}
 ];
 
 @NgModule({
--- app/src/app/pages/deals/deals.module.ts
+++ app/src/app/pages/deals/deals.module.ts
@@ -6,9 +6,10 @@ import {DealListComponent} from './deal-list/deal-list.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {CurrencyMaskModule} from "ng2-currency-mask";
 import {DealDetailComponent} from "./deal-detail/deal-detail.component";
+import {DealFormComponent} from "./deal-form/deal-form.component";
 
 @NgModule({
-  declarations: [DealListComponent, DealDetailComponent],
+  declarations: [DealListComponent, DealDetailComponent, DealFormComponent],
   imports: [
     CommonModule,
     DealsRoutingModule,
